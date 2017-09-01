f2e1210b3d1634479538663edf9f81c43e7c9a4a
PsychoTea <bensparkes8@gmail.com>
2017-09-01 23:18:52 +0100

    Added files to csproj, removed classes from Message.cs

--- Oxide.Ext.Discord/Libraries/DiscordObjects/Message.cs
+++ Oxide.Ext.Discord/Libraries/DiscordObjects/Message.cs
@@ -9,39 +9,6 @@ namespace Oxide.Ext.Discord.Libraries.DiscordObjects
 {
     public class Message
     {
-        public class Thumbnail
-        {
-            public int width { get; set; }
-            public string url { get; set; }
-            public string proxy_url { get; set; }
-            public int height { get; set; }
-        }
-
-        public class Provider
-        {
-            public string url { get; set; }
-            public string name { get; set; }
-        }
-
-        public class Embed
-        {
-            public string url { get; set; }
-            public string type { get; set; }
-            public string title { get; set; }
-            public Thumbnail thumbnail { get; set; }
-            public Provider provider { get; set; }
-            public string description { get; set; }
-            public Author author { get; set; }
-        }
-
-        public class Author
-        {
-            public string username { get; set; }
-            public string id { get; set; }
-            public string discriminator { get; set; }
-            public bool bot { get; set; }
-            public string avatar { get; set; }
-        }
         public string webhook_id { get; set; }
         public int type { get; set; }
         public bool tts { get; set; }
