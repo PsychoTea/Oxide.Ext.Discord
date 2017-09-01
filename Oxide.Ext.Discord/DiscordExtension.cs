ee7703ea9848bb3ff74cda26e9e83c2a00bf6016
PsychoTea <bensparkes8@gmail.com>
2017-09-01 01:54:09 +0100

    [DiscordExtension] Minor changes

--- Oxide.Ext.Discord/DiscordExtension.cs
+++ Oxide.Ext.Discord/DiscordExtension.cs
@@ -9,26 +9,13 @@ namespace Oxide.Ext.Discord
     {
         internal static readonly Version AssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
 
-        public DiscordExtension(ExtensionManager manager) : base(manager)
-        {
-        }
+        public DiscordExtension(ExtensionManager manager) : base(manager) { }
 
         public override string Name => "Discord";
         public override string Author => "DylanSMR";
-
         public override VersionNumber Version => new VersionNumber(AssemblyVersion.Major, AssemblyVersion.Minor, AssemblyVersion.Build);
-        private Libraries.Discord connection;
-        public override void Load()
-        {
-            connection = new Libraries.Discord(this);
-            Manager.RegisterLibrary("Discord", connection);
-        }
-        public override void LoadPluginWatchers(string plugindir)
-        {
-        }
 
-        public override void OnModLoad()
-        {
-        }
+        private Libraries.Discord discord;
+        public override void Load() => Manager.RegisterLibrary("Discord", discord = new Libraries.Discord(this));
     }
 }
