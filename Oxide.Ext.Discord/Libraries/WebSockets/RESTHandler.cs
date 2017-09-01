7e4c4162d9dbcb5764e7aebb983f23f4b273a389
PsychoTea <bensparkes8@gmail.com>
2017-09-01 16:01:52 +0100

    So many changes I forgot what I did

--- Oxide.Ext.Discord/Libraries/WebSockets/RESTHandler.cs
+++ Oxide.Ext.Discord/Libraries/WebSockets/RESTHandler.cs
@@ -23,55 +23,71 @@ namespace Oxide.Ext.Discord.Libraries.WebSockets
 
         public void GetChannel(string channelID, Action<Server.Channel> callback)
         {
-            string URL = $"{URLBase}/channeles/{channelID}";
-            WebRequest.EnqueueGet(URL, (code, response) =>
-            {
-                if (!VerifyRequest(code, response)) return;
-
-                var channel = JsonConvert.DeserializeObject<Server.Channel>(response);
-                callback.Invoke(channel);
-            }, null, headers);
+            string URL = $"{URLBase}/channels/{channelID}";
+            DoGet(URL, typeof(Server.Channel), (obj) => callback.Invoke(obj as Server.Channel));
         }
 
-        public void ModifyChannel(string channelID, Server.Channel channel, Action<Server.Channel> callback)
+        public void ModifyChannel(string channelID, Server.Channel channel, Action<Server.Channel> callback = null)
         {
-            string URL = $"{URLBase}/channe;s/{channelID}";
-            string body = JsonConvert.SerializeObject(channel);
-            WebRequest.EnqueuePut(URL, body, (code, response) =>
-            {
-                if (!VerifyRequest(code, response)) return;
-
-                var retChannel = JsonConvert.DeserializeObject<Server.Channel>(response);
-                callback?.Invoke(retChannel);
-            }, null, headers);
+            string URL = $"{URLBase}/channels/{channelID}";
+            DoPut(URL, channel, (obj) => callback?.Invoke(obj as Server.Channel));
         }
 
         public void CreateMessage(string channelID, Message message, Action<Message> callback = null)
         {
             string URL = $"{URLBase}/channels/{channelID}/messages";
-            string body = JsonConvert.SerializeObject(message);
-            WebRequest.EnqueuePost(URL, body, (code, response) =>
-            {
-                if (!VerifyRequest(code, response)) return;
-
-                var retMessage = JsonConvert.DeserializeObject<Message>(response);
-                callback?.Invoke(retMessage);
-            }, null, headers);
+            DoPost(URL, message, (obj) => callback?.Invoke(obj as Message));
         }
 
         #endregion
+
+        private void DoGet(string URL, Type returnType = null, Action<object> callback = null)
+        {
+            WebRequest.EnqueueGet(URL, (code, response) =>
+            {
+                if (!VerifyRequest(code, response)) return;
+
+                if (returnType == null) return;
+                var responseObj = JsonConvert.DeserializeObject(response, returnType);
+                callback?.Invoke(responseObj);
+            }, null, headers);
+        }
+
+        private void DoPut(string URL, object data, Action<object> callback = null)
+        {
+            string contents = JsonConvert.SerializeObject(data);
+            WebRequest.EnqueuePut(URL, contents, (code, response) =>
+            {
+                if (!VerifyRequest(code, response)) return;
+                
+                var responseObj = JsonConvert.DeserializeObject(response, data.GetType());
+                callback?.Invoke(responseObj);
+            }, null, headers);
+        }
+
+        private void DoPost(string URL, object data, Action<object> callback = null)
+        {
+            string contents = JsonConvert.SerializeObject(data);
+            WebRequest.EnqueuePost(URL, contents, (code, response) =>
+            {
+                if (!VerifyRequest(code, response)) return;
+                
+                var responseObj = JsonConvert.DeserializeObject(response, data.GetType());
+                callback?.Invoke(responseObj);
+            }, null, headers);
+        }
 
         private bool VerifyRequest(int code, string response)
         {
             if (code != 200)
             {
-                Interface.Oxide.LogError($"[Discord Ext] Recieved code {code} from Discord API (Response: {response}).");
+                Interface.Oxide.LogError($"[Discord Ext] Received code {code} from Discord API (Response: {response}).");
                 return false;
             }
 
             if (response == null)
             {
-                Interface.Oxide.LogError($"[Discord Ext] Recieved a null response from Discord API (Code: {code}).");
+                Interface.Oxide.LogError($"[Discord Ext] Received a null response from Discord API (Code: {code}).");
                 return false;
             }
 
