30b633ab10779269d7862d56d5e0f48669b50345
DylanSMR <dylansmrw@gmail.com>
2017-08-31 21:24:34 -0500

    Added files for sending messages to discord.

---
+++ Oxide.Ext.Discord/Libraries/SocketObjects/DiscordPayload.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+namespace Oxide.Ext.Discord.Libraries.SocketObjects
+{
+    public class DiscordPayload
+    {
+        [JsonProperty("content")]
+        public string MessageText { get; set; }
+    }
+}
