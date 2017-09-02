56440bb34b43b714dac7363a981b9c8e209af9a3
DylanSMR <dylansmrw@gmail.com>
2017-09-02 18:33:05 -0500

    Add files via upload

---
+++ Oxide.Ext.Discord/Exceptions/UpkeepFailedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oxide.Ext.Discord.Exceptions
+{
+    public class UpkeepFailedException : Exception
+    {
+        public UpkeepFailedException(string apiKey) : base($"Api Key: {apiKey} has failed to keep up the connection with the extension!") { }
+    }
+}
