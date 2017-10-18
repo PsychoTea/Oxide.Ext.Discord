b26255738f48b4ff61159bd5ee94da20a0446e57
PsychoTea <bensparkes8@gmail.com>
2017-10-18 16:16:35 +0100

    [SocketRunningException] Added Client plugin name

--- Oxide.Ext.Discord/Exceptions/SocketRunningException.cs
+++ Oxide.Ext.Discord/Exceptions/SocketRunningException.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
+using Oxide.Ext.Discord.WebSockets;
 
 namespace Oxide.Ext.Discord.Exceptions
 {
     public class SocketRunningException : Exception
     {
-        public SocketRunningException() : base("Error! Tried to create a socket when one is already running.") { }
+        public SocketRunningException(DiscordClient client) : base($"Error! Tried to create a socket when one is already running @ {client.GetPluginNames()}") { }
     }
 }
