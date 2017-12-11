b46fabe618410b05ed1de344c2e957e82c091f0b
PsychoTea <bensparkes8@gmail.com>
2017-12-11 20:34:44 +0000

    [DiscordExtension] commented (exp) 'SupportsReloading' Oxide feature

--- Oxide.Ext.Discord/DiscordExtension.cs
+++ Oxide.Ext.Discord/DiscordExtension.cs
@@ -14,6 +14,8 @@
         {
         }
 
+        //public override bool SupportsReloading => true;
+
         public override string Name => "Discord";
 
         public override string Author => "PsychoTea & DylanSMR";
