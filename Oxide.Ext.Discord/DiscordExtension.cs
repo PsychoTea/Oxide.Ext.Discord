2758db35b867462a7681e8cfea16f2a9edbe31b7
DylanSMR <dylansmrw@gmail.com>
2018-02-09 23:20:32 -0600

    Added OnPluginUnload handling, and new optional settings class.

--- Oxide.Ext.Discord/DiscordExtension.cs
+++ Oxide.Ext.Discord/DiscordExtension.cs
@@ -5,6 +5,7 @@
     using System.Reflection;
     using Oxide.Core;
     using Oxide.Core.Extensions;
+    using Oxide.Core.Plugins;
 
     public class DiscordExtension : Extension
     {
@@ -26,6 +27,33 @@
             {
                 Interface.Oxide.LogException("An exception was thrown!", exception.ExceptionObject as Exception);
             };
+        }
+
+        [HookMethod("OnPluginUnloaded")]
+        void OnPluginUnloaded(Plugin name)
+        {
+            foreach (var client in new List<DiscordClient>(Discord.Clients))
+            {
+                if (client.OptionalSettings.KeepLoadedAfterUnload) continue;
+                if (client.Plugins.Count == 1)
+                {
+                    if (client.Plugins[0] != name) continue;
+                    if (client.OptionalSettings.ExtensiveLogging) Interface.Oxide.LogWarning($"[Discord Ext] Shutting down discord bot registered for plugin {name.Name} as it was set to disconnect upon unload.");
+                    Discord.CloseClient(client);
+                    continue;
+                }
+                if(client.Plugins.Count == 0)
+                {
+                    Discord.CloseClient(client);
+                    continue;
+                }
+
+                if (client.Plugins.Contains(name))
+                {
+                    if (client.OptionalSettings.ExtensiveLogging) Interface.Oxide.LogWarning($"[Discord Ext] Handling plugin unload for plugin {name.Name}.");
+                    client.Plugins.Remove(name);
+                }
+            }
         }
 
         public override void OnShutdown()
