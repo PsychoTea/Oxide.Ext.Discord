ef7d0a639c15cb0ba175abac08477cb137cf215b
Ben Sparkes <bensparkes8@gmail.com>
2018-06-02 22:36:58 +0100

    Added ChannelType/MessageType enums

---
+++ Oxide.Ext.Discord/DiscordObjects/ChannelType.cs
@@ -0,0 +1,11 @@
+﻿namespace Oxide.Ext.Discord.DiscordObjects
+{
+    public enum ChannelType
+    {
+        GuildText = 0,
+        DM = 1,
+        GuildVoice = 2,
+        GroupDM = 3,
+        GuildCategory = 4
+    }
+}
