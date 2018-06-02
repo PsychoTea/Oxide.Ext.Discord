39729d11d56fa5190b1632a1374b32a69453e7fd
Ben Sparkes <bensparkes8@gmail.com>
2018-06-02 22:00:22 +0100

    Added new exceptions

---
+++ Oxide.Ext.Discord/Exceptions/SettingsNullException.cs
@@ -0,0 +1,11 @@
+﻿namespace Oxide.Ext.Discord.Exceptions
+{
+    using System;
+
+    public class SettingsNullException : Exception
+    {
+        public SettingsNullException() : base("Error! Please supply a valid settings object!")
+        {
+        }
+    }
+}
