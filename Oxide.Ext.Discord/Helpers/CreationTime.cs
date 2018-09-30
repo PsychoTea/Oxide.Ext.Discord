5d4c6603debaefc32e7540602535667c45a18930
Slydelix <slydelix@gmail.com>
2018-09-30 22:31:16 +0200

    Add user creation date to helpers

---
+++ Oxide.Ext.Discord/Helpers/CreationTime.cs
@@ -0,0 +1,19 @@
+﻿namespace Oxide.Ext.Discord.Helpers
+{
+    using Oxide.Ext.Discord.DiscordObjects;
+    using System;
+
+    public class CreationTime
+    {
+        public static DateTime GetFromUser(User user) => GetFromUserID(user.id);
+
+        public static DateTime GetFromUserID(string userID)
+        {
+            long id = long.Parse(userID);
+
+            long ageInSeconds = ((id >> 22) + 1420070400000) / 1000;
+
+            return new DateTime(1970, 1, 1).AddSeconds(ageInSeconds);
+        }
+    }
+}
