0e45e2d36051126ae9ed7f57b2680299cc780ffc
Austin Vandersluis <austinv900@users.noreply.github.com>
2019-02-06 00:13:13 -0800

    Add Watching to ActivityType.cs

--- Oxide.Ext.Discord/DiscordObjects/ActivityType.cs
+++ Oxide.Ext.Discord/DiscordObjects/ActivityType.cs
@@ -1,9 +1,10 @@
-﻿namespace Oxide.Ext.Discord.DiscordObjects
+namespace Oxide.Ext.Discord.DiscordObjects
 {
     public enum ActivityType
     {
         Game = 0,       // Playing {name}
         Streaming = 1,  // Streaming {name}
-        Listening = 2   // Listening {name}
+        Listening = 2,   // Listening {name}
+        Watching = 3 // Watching {name}
     }
 }
